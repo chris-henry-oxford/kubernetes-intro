264403a88250adbd26e6b0d1a15238bb3e933798
chris-henry-oxford <47182814+chris-henry-oxford@users.noreply.github.com>
2019-01-30 14:43:55 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,17 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Chris Henry", "location": "Oxford"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "2009", "yearTo": "2019", "place": "Oxford Computer Consultants", "comment": "Tech Lead"}, {"yearFrom": "2001", "yearTo": "2008", "place": "Oxford Computer Consultants", "comment": "Developer"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
 app.get('/api/education', (request, response) => {
-	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]');
+	response.send('[{"yearFrom": "1997", "yearTo": "2001", "place": "Oxford Earth Sciences", "comment": "Earthquakes"}, {"yearFrom": "1993", "yearTo": "1997", "place": "Cambridge", "comment": "Physics"}]');
 	logger.info('Responded to GET request on /api/edu')
 });
 
